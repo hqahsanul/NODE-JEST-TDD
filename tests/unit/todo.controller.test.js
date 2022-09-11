23e596264e4aad7cb0bc496dae95bc063cf2f216
hqahsanul <hqahsanul@gmail.com>
2022-09-11 23:51:17 +0530

    added mock environment

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,7 +1,16 @@
 const TodoController=require("../../controllers/todo.controller");
 const TodoModel=require("../../model/todo.model");
+const httpMocks=require("node-mocks-http");
+const newTodo=require("../mock-data/new-todo.json")
 
 TodoModel.create=jest.fn();
+
+let req,res,next;
+beforeEach(()=>{
+    req=httpMocks.createRequest();
+    res=httpMocks.createResponse();
+    next=null;
+})
 
 
 describe("TodoController.createTodo", ()=>{
@@ -10,7 +19,10 @@ describe("TodoController.createTodo", ()=>{
     });
 
     it("should call TodoModel.create",()=>{
-        TodoController.createTodo();
-        expect(TodoModel.create).toBeCalled();
+
+        req.body=newTodo;
+        TodoController.createTodo(req,res,next);
+        expect(TodoModel.create).toBeCalledWith(newTodo);
+
     })
 });
